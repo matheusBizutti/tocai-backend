b4dfe2e3c59bb663889940a772ed16a768cc8440
Matheus Bizutti <matheusbizutti@Matheuss-MacBook-Pro.local>
2018-11-23 20:34:42 -0200

    removed comments

--- controllers/musical-list/musical-list.controller.js
+++ controllers/musical-list/musical-list.controller.js
@@ -6,9 +6,26 @@ exports.musical_list_all = (req, res) => {
 
 };
 
+exports.musical_list_email = (req, res) => {
+
+  getMusicalListByEmail(req, res);
+
+};
+
+
 function getMusicalListByType(req, res) {
   
   const query  = AuthenticateSignupPartnersSchema.where({ typeuser: req.params.typeuser }); 
+
+  query.find((err, list) => {
+    if (err) return res.send(err)
+    res.json(list);
+  });
+};
+
+function getMusicalListByEmail(req, res) {
+  
+  const query  = AuthenticateSignupPartnersSchema.where({ email: req.params.email }); 
 
   query.find((err, list) => {
     if (err) return res.send(err)
--- routes/musical-list/musical-list.route.js
+++ routes/musical-list/musical-list.route.js
@@ -3,18 +3,8 @@ const router = express.Router();
 
 const musical_list_controller = require('../../controllers/musical-list/musical-list.controller');
 
-// router.get('/:id', meeting_room_controller.meeting_room_details);
-
 router.get('/list/:typeuser', musical_list_controller.musical_list_all);
 
-// router.get('/list/:init/:end', meeting_room_controller.meeting_room_meetings);
-
-// router.post('/create', meeting_room_controller.meeting_room_create);
-
-// router.put('/:id/update', meeting_room_controller.meeting_room_update);
-
-// router.delete('/:id/delete', meeting_room_controller.meeting_room_delete);
-
-// router.delete('/deleteAll', meeting_room_controller.meeting_room_deleteAll);
+router.get('/email/:email', musical_list_controller.musical_list_email);
 
 module.exports = router;
